660d2edd1bc19ad49c0f170d6755e698f9488dff
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2022-12-09 10:34:29 +0500

    initial commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Todo app!</h1>
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
